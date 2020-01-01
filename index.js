96f47a8b22ddd3a4972c0731bc1b59dcfe3e3164
nakajima1412 <nakajima1412@icloud.com>
2020-01-01 14:22:22 +0900

    HTTP通信セクション

---
+++ index.js
@@ -0,0 +1,19 @@
+function fetchUserInfo (userId) {
+  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then(response => {
+      console.log (response.status);
+      // エラーレスポンスが返されたことを検知する
+      if (!response.ok) {
+        console.error('エラーレスポンス', response);
+      } else {
+        return response.json().then(userInfo => {
+          // JSONパースされたオブジェクトが渡される
+          console.log(userInfo);
+        });
+      }
+    }).catch (error => {
+      console.error(error);
+    });
+}
+
+console.log('index.js: loaded');
